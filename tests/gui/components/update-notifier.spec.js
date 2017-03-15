60e2d1e8032a1891ccdb99b2cf1fb1c9b1632c29
Juan Cruz Viotti <jviotti@openmailbox.org>
2017-03-15 11:39:24 -0400

    refactor(GUI): integrate `etcher-latest-version` into the main repo
    `etcher-latest-version` was kept in a separate repository in order to
    re-use it with the Etcher website, however the Etcher website is not
    using it at all, and we're moving towards having the website in the main
    repository.
    
    Therefore, this commit brings back the logic from
    `etcher-latest-version`, but introduces it as
    `lib/shared/s3-packages.js`, in order to not tie ourselves to the
    AngularJS framework, and as a step towards the Etcher SDK.
    
    As a nice little bonus, this commit adds support for an
    `ETCHER_FAKE_S3_LATEST_VERSION` environment variable that can be used to
    trick Etcher that there is an available update, and therefore show the
    update notifier modal.
    
    Also, this commit adds support for snapshot builds update-checks, by
    checking the `resin-nightly-downloads` S3 bucket if the current version
    contains a git commit hash build number.
    
    If the version is not a production release, then the update notifier
    modal doesn't present the checkbox to disable update notifications for X
    days.
    
    See: https://github.com/resin-io-modules/etcher-latest-version
    Fixes: https://github.com/resin-io/etcher/issues/953
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

--- tests/gui/components/update-notifier.spec.js
+++ tests/gui/components/update-notifier.spec.js
@@ -3,6 +3,7 @@
 const m = require('mochainon');
 const angular = require('angular');
 const units = require('../../../lib/shared/units');
+const release = require('../../../lib/shared/release');
 const settings = require('../../../lib/gui/models/settings');
 require('angular-mocks');
 
@@ -24,177 +25,106 @@ describe('Browser: UpdateNotifier', function() {
         UPDATE_NOTIFIER_SLEEP_DAYS = _UPDATE_NOTIFIER_SLEEP_DAYS_;
       }));
 
-      describe('given the `sleepUpdateCheck` is disabled', function() {
+      describe('given a production release type', function() {
 
         beforeEach(function() {
-          settings.set('sleepUpdateCheck', false);
+          this.releaseType = release.RELEASE_TYPE.PRODUCTION;
         });
 
-        it('should return true', function() {
-          const result = UpdateNotifierService.shouldCheckForUpdates();
-          m.chai.expect(result).to.be.true;
-        });
-
-      });
-
-      describe('given the `sleepUpdateCheck` is enabled', function() {
-
-        beforeEach(function() {
-          settings.set('sleepUpdateCheck', true);
-        });
-
-        describe('given the `lastUpdateNotify` was never updated', function() {
+        describe('given the `sleepUpdateCheck` is disabled', function() {
 
           beforeEach(function() {
-            settings.set('lastUpdateNotify', undefined);
+            settings.set('sleepUpdateCheck', false);
           });
 
           it('should return true', function() {
-            const result = UpdateNotifierService.shouldCheckForUpdates();
+            const result = UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
             m.chai.expect(result).to.be.true;
           });
 
         });
 
-        describe('given the `lastUpdateNotify` was very recently updated', function() {
+        describe('given the `sleepUpdateCheck` is enabled', function() {
 
           beforeEach(function() {
-            settings.set('lastUpdateNotify', Date.now() + 1000);
+            settings.set('sleepUpdateCheck', true);
           });
 
-          it('should return false', function() {
-            const result = UpdateNotifierService.shouldCheckForUpdates();
-            m.chai.expect(result).to.be.false;
+          describe('given the `lastUpdateNotify` was never updated', function() {
+
+            beforeEach(function() {
+              settings.set('lastUpdateNotify', undefined);
+            });
+
+            it('should return true', function() {
+              const result = UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
+              m.chai.expect(result).to.be.true;
+            });
+
+          });
+
+          describe('given the `lastUpdateNotify` was very recently updated', function() {
+
+            beforeEach(function() {
+              settings.set('lastUpdateNotify', Date.now() + 1000);
+            });
+
+            it('should return false', function() {
+              const result = UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
+              m.chai.expect(result).to.be.false;
+            });
+
+          });
+
+          describe('given the `lastUpdateNotify` was updated long ago', function() {
+
+            beforeEach(function() {
+              const SLEEP_MS = units.daysToMilliseconds(UPDATE_NOTIFIER_SLEEP_DAYS);
+              settings.set('lastUpdateNotify', Date.now() + SLEEP_MS + 1000);
+            });
+
+            it('should return true', function() {
+              const result = UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
+              m.chai.expect(result).to.be.true;
+            });
+
+            it('should unset the `sleepUpdateCheck` setting', function() {
+              m.chai.expect(settings.get('sleepUpdateCheck')).to.be.true;
+              UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
+              m.chai.expect(settings.get('sleepUpdateCheck')).to.be.false;
+            });
+
           });
 
         });
 
-        describe('given the `lastUpdateNotify` was updated long ago', function() {
+      });
+
+      describe('given a snapshot release type', function() {
+
+        beforeEach(function() {
+          this.releaseType = release.RELEASE_TYPE.SNAPSHOT;
+        });
+
+        describe('given the `sleepUpdateCheck` is enabled', function() {
 
           beforeEach(function() {
-            const SLEEP_MS = units.daysToMilliseconds(UPDATE_NOTIFIER_SLEEP_DAYS);
-            settings.set('lastUpdateNotify', Date.now() + SLEEP_MS + 1000);
+            settings.set('sleepUpdateCheck', true);
           });
 
-          it('should return true', function() {
-            const result = UpdateNotifierService.shouldCheckForUpdates();
-            m.chai.expect(result).to.be.true;
+          describe('given the `lastUpdateNotify` was very recently updated', function() {
+
+            beforeEach(function() {
+              settings.set('lastUpdateNotify', Date.now() + 1000);
+            });
+
+            it('should return true', function() {
+              const result = UpdateNotifierService.shouldCheckForUpdates(this.releaseType);
+              m.chai.expect(result).to.be.true;
+            });
+
           });
 
-          it('should unset the `sleepUpdateCheck` setting', function() {
-            m.chai.expect(settings.get('sleepUpdateCheck')).to.be.true;
-            UpdateNotifierService.shouldCheckForUpdates();
-            m.chai.expect(settings.get('sleepUpdateCheck')).to.be.false;
-          });
-
-        });
-
-      });
-
-    });
-
-    describe('.isLatestVersion()', function() {
-
-      describe('given the latest version is equal to the current version', function() {
-
-        let $q;
-        let $rootScope;
-        let UpdateNotifierService;
-        let ManifestBindService;
-
-        beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_, _ManifestBindService_) {
-          $q = _$q_;
-          $rootScope = _$rootScope_;
-          UpdateNotifierService = _UpdateNotifierService_;
-          ManifestBindService = _ManifestBindService_;
-        }));
-
-        beforeEach(function() {
-          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
-          this.getLatestVersionStub.returns($q.resolve(ManifestBindService.get('version')));
-        });
-
-        afterEach(function() {
-          this.getLatestVersionStub.restore();
-        });
-
-        it('should resolve true', function() {
-          let result = null;
-
-          UpdateNotifierService.isLatestVersion().then(function(isLatestVersion) {
-            result = isLatestVersion;
-          });
-
-          $rootScope.$apply();
-          m.chai.expect(result).to.be.true;
-        });
-
-      });
-
-      describe('given the latest version is greater than the current version', function() {
-
-        let $q;
-        let $rootScope;
-        let UpdateNotifierService;
-
-        beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_) {
-          $q = _$q_;
-          $rootScope = _$rootScope_;
-          UpdateNotifierService = _UpdateNotifierService_;
-        }));
-
-        beforeEach(function() {
-          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
-          this.getLatestVersionStub.returns($q.resolve('99999.9.9'));
-        });
-
-        afterEach(function() {
-          this.getLatestVersionStub.restore();
-        });
-
-        it('should resolve false', function() {
-          let result = null;
-
-          UpdateNotifierService.isLatestVersion().then(function(isLatestVersion) {
-            result = isLatestVersion;
-          });
-
-          $rootScope.$apply();
-          m.chai.expect(result).to.be.false;
-        });
-
-      });
-
-      describe('given the latest version is less than the current version', function() {
-
-        let $q;
-        let $rootScope;
-        let UpdateNotifierService;
-
-        beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_) {
-          $q = _$q_;
-          $rootScope = _$rootScope_;
-          UpdateNotifierService = _UpdateNotifierService_;
-        }));
-
-        beforeEach(function() {
-          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
-          this.getLatestVersionStub.returns($q.resolve('0.0.0'));
-        });
-
-        afterEach(function() {
-          this.getLatestVersionStub.restore();
-        });
-
-        it('should resolve true', function() {
-          let result = null;
-
-          UpdateNotifierService.isLatestVersion().then(function(isLatestVersion) {
-            result = isLatestVersion;
-          });
-
-          $rootScope.$apply();
-          m.chai.expect(result).to.be.true;
         });
 
       });
